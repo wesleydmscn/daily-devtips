2a31fbc8e7606f7e237edf928329cb0f8dc53603
Wesley Damasceno <wesleydmscno@gmail.com>
2024-12-17 21:03:56 -0300

    refactor: add script test:ci in package.json and update e2e app.spec.ts

--- server/test/e2e/app.spec.ts
+++ server/test/e2e/app.spec.ts
@@ -1,11 +1,26 @@
+import { Server } from 'http';
 import supertest from 'supertest';
+
+import { dataSource } from '@/config/typeorm.config';
 import app from '@/app';
 
 describe('GET /', () => {
+  let server: Server;
+
+  beforeAll(async () => {
+    await dataSource.initialize();
+    server = app.listen();
+  });
+
   it('should return message: "Hello World"', async () => {
     const res = await supertest(app).get('/');
 
     expect(res.statusCode).toBe(200);
     expect(res.text).toStrictEqual('Hello World!');
+  });
+
+  afterAll(async () => {
+    await dataSource.destroy();
+    server.close();
   });
 });
