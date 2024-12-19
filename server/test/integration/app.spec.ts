c2cbd422690643c728311a87ab095a8c03da5c9f
Wesley Damasceno <wesleydmscno@gmail.com>
2024-12-19 14:36:30 -0300

    test: refactor integration tests for improved readability and consistency

--- server/test/integration/app.spec.ts
+++ server/test/integration/app.spec.ts
@@ -6,18 +6,18 @@ import app from '@/app';
 describe('GET /', () => {
   let server: Server;
 
-  beforeAll(async () => {
+  beforeAll(() => {
     server = app.listen();
   });
 
-  it('should return message: "Hello World"', async () => {
+  it('should return a message saying "Hello World!"', async () => {
     const res = await supertest(app).get('/api/');
 
     expect(res.statusCode).toBe(200);
     expect(res.text).toStrictEqual('Hello World!');
   });
 
-  afterAll(async () => {
+  afterAll(() => {
     server.close();
   });
 });
