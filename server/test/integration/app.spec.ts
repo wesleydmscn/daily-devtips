d79805b9cf1df0b5c1501dd7074132202d0ee416
Wesley Damasceno <wesleydmscno@gmail.com>
2024-12-19 10:19:29 -0300

    refactor: rename server ci to server-tests and update package.json scripts and app.spec.ts

--- server/test/integration/app.spec.ts
+++ server/test/integration/app.spec.ts
@@ -1,26 +1,23 @@
 import { Server } from 'http';
 import supertest from 'supertest';
 
-import { dataSource } from '@/config/typeorm.config';
 import app from '@/app';
 
 describe('GET /', () => {
   let server: Server;
 
   beforeAll(async () => {
-    await dataSource.initialize();
     server = app.listen();
   });
 
   it('should return message: "Hello World"', async () => {
-    const res = await supertest(app).get('/');
+    const res = await supertest(app).get('/api/');
 
     expect(res.statusCode).toBe(200);
     expect(res.text).toStrictEqual('Hello World!');
   });
 
   afterAll(async () => {
-    await dataSource.destroy();
     server.close();
   });
 });
